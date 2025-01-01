f337f7d8d8b66e369777cc073dcfc9f2c9bedab7
JC.Gaytan <juan.gaytan@inizio.com>
2024-12-31 19:10:34 -0500

    Initial Commit

---
+++ CoinCraze/CoinCrazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCrazeApp.swift
+//  CoinCraze
+//
+//  Created by Juan Carlos Gaytan Delgado on 2024-12-31.
+//
+
+import SwiftUI
+
+@main
+struct CoinCrazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinCraze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinCraze
+//
+//  Created by Juan Carlos Gaytan Delgado on 2024-12-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
